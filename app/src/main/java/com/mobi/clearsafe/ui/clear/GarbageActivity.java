b5786f12a4b12fa4965fe5d2c844a6ae7340536c
zhousaito <zhousaito@163.com>
2020-03-20 10:40:45 +0800

    清理垃圾和电池冷却activity创建

---
+++ app/src/main/java/com/mobi/clearsafe/ui/clear/GarbageActivity.java
@@ -0,0 +1,16 @@
+package com.mobi.clearsafe.ui.clear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mobi.clearsafe.R;
+
+public class GarbageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_garbage);
+
+    }
+}
---
+++ app/src/main/java/com/mobi/clearsafe/ui/clear/PowerCoolActivity.java
@@ -0,0 +1,19 @@
+package com.mobi.clearsafe.ui.clear;
+
+import android.os.Bundle;
+
+import com.mobi.clearsafe.R;
+import com.mobi.clearsafe.base.BaseAppCompatActivity;
+
+/**
+ * 降温
+ */
+public class PowerCoolActivity extends BaseAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_power_cool);
+
+    }
+}
